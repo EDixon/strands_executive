98be75c5a4350955888ebc8e61ef04b61b2f6b3a
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-08-11 18:50:35 +0000

    first stuff for policy adding

--- mdp_plan_exec/scripts/test_client.py
+++ mdp_plan_exec/scripts/test_client.py
@@ -5,11 +5,31 @@ import rospy
 import rospkg
 
 from mdp_plan_exec.mdp import TopMapMdp, ProductMdp
+from mdp_plan_exec.prism_mdp_manager import PrismMdpManager
+
     
 class MdpPlanner(object):
+    
+    
     def main(self):     
-        top_map_mdp = TopMapMdp('cs_lg_large')
-        top_map_mdp.update_nav_statistics()
+        manager = PrismMdpManager(8085, 'eliot_dir', 'cs_lg_large2')
+        manager.top_map_mdp.update_nav_statistics()
+        
+        target_waypoint = "WayPoint4"
+        
+
+        
+        specification='R{"time"}min=? [ (F "' + target_waypoint + '") ]'
+        expected_time=float(manager.prism_client.get_policy("all_day",specification))
+        result_dir=manager.get_working_dir() + '/all_day'
+        
+        manager.top_map_mdp.set_reachability_policy(result_dir + "/adv.tra", result_dir + "/prod.sta")
+        
+        manager.prism_client.shutdown(False)
+        
+        
+        
+    
 
 if __name__ == '__main__':
     rospy.init_node('test_client')
